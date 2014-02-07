da1733c936fc1bdc35111ad85ac2a85733af7d44
stevenspiel <stevenspiel@gmail.com>
2014-02-07 16:00:13 -0600

    add button js functionality to survey taking

--- public/js/application.js
+++ public/js/application.js
@@ -7,15 +7,41 @@ $(document).ready(function () {
     request.done(function () { window.location = "/"; });
   });
 
+  // survey taking actions
+
+  var answers = {};
+
   $(".answer").click(function(){
-    console.log("MUCH CLICKS");
+    // console.log("MUCH CLICKS");
     $("#next-question").removeAttr("disabled");
+    if (parseInt($(".doge-frames").css("left").replace("px", "")) === ($(".doge-item").length * -400 + 400)) {
+      $("#next-question").html("Submit");
+      answers.title = $("#survey-title").html();
+      $("#next-question").click(function(){
+        $.ajax({
+          type: "POST",
+          url: "/survey",
+          data: answers,
+          success: function(){
+            window.location.href = "/thanks"
+          },
+          dataType: "json"
+        });
+      });
+    }
   });
 
   $("#next-question").click(function(){
-    console.log("MANY NEXT");
+    // console.log("MANY NEXT");
     $(".doge-frames").css("left", "-=" + 400);
     $("#next-question").attr("disabled", true);
   });
+
+  $(".doge-frames").css("width", ($(".doge-item").length * 100) + "%");
+
+  $(".doge-item").css("width", (100 / $(".doge-item").length) + "%");
+
+  // survey creating actions
+
 
 });
