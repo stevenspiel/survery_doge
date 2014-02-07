826b30e62009f93355d6832a3865c4e5bef13dc4
Sam and Daniel <sblackman@gmail.com>
2014-02-07 11:05:32 -0600

    Add associations to models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,3 @@
 class Answer < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :question
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,6 @@
 class Survey < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  has_many :questions
+  has_many :survey_events
+  has_many :answers, through: :questions
 end
