9e0577c17be60e3f28d60852879080220ef30137
Daniel Lu <dandydanny@gmail.com>
2014-02-07 14:08:58 -0600

    Initialize vote_count to 0, implement vote_count increment in controller

--- app/controllers/survery.rb
+++ app/controllers/survery.rb
@@ -11,3 +11,12 @@ get '/survey/stats/:id' do
   @survey = Survey.find(params[:id])
   erb :stats
 end
+
+post '/survey' do
+  # we expect array in param
+  params[:answers].each do |id|
+    # increment vote count on each answer based on ques. number coming in
+    a = Answer.find(id).vote_count+=1
+    a.save
+  end
+end
