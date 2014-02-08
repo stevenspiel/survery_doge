813063199941213363f21d1205b8f3a45cf964be
Brian Murphy <bmurphy1@mac.com>
2014-02-08 12:33:59 -0600

    Fix merge of send json code

--- public/js/application.js
+++ public/js/application.js
@@ -50,10 +50,7 @@ $(document).ready(function () {
 
 
   // Survey creation functions
-<<<<<<< HEAD
-  var completeSurvey = {title: "doge magic"};
-=======
->>>>>>> a3c578f0d32431d076d3cf33b95d1c389dcbdf54
+  var completeSurvey = {title: "A Doges Life"};
 
   var addDiv = $('#new-question-container');
   var i = $('#new-question-container p').length + 1;
@@ -67,10 +64,7 @@ $(document).ready(function () {
   });
 
 
-
-  var completeSurvey = {
-    title: "A Doges Life"
-  };
+  // Send completed survey object to server as json
 
   $("#submit").click(function(e) {
     e.preventDefault();
