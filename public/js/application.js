df8e4a44bc3954e05b42f9005717ab2d0c71b30a
Sam Blackman <sblackman@gmail.com>
2014-02-08 17:08:14 -0600

    create js object containing title and all questions and options

--- public/js/application.js
+++ public/js/application.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
   });
 
   $("#next-question").click(function(){
-    // console.log("MANY NEXT");
+    console.log("MANY NEXT");
     // window.Answers.
     window.Answers.answers.push($("input[type=radio]:checked").last().attr("value"));
     $(".doge-frames").css("left", "-=" + 400);
@@ -47,11 +47,17 @@ $(document).ready(function () {
 
   $(".doge-item").css("width", (100 / $(".doge-item").length) + "%");
 
+
+
 });
 
 
 
 // Survey creation functions
+
+var completeSurvey = {};
+
+var questionsArray = [];
 
 $(document).on('click','#new-option',function(){
   var iO = $('#new-question-container p').length + 1;
@@ -64,10 +70,14 @@ $(document).on('click','#new-option',function(){
 });
 
 $(document).on('click','#next',function(){
-  console.log($(".new-question").val());
-  for (var i = $(".new-answer").length - 1; i >= 0; i--) {
-    console.log($($(".new-answer")[i]).val());
+  var q_name = $(".new-question ").val();
+  console.log("Length of new answers is" + $(".new-answer").length);
+  for (var i = 0; i < $(".new-answer").length; i++) {;
+    questionsArray[i] = $($(".new-answer")[i]).val();
   };
+  console.log(questionsArray);
+  completeSurvey[q_name] = questionsArray;
+  console.log(completeSurvey);
   window.newSurveyHTML = $("#question-and-buttons").html();
   var iQ = parseInt($("input[tag='question']").attr('name').replace('question',''));
   $("#question-and-buttons").html(window.newSurveyHTML);
@@ -79,6 +89,11 @@ $(document).on('click','#next',function(){
   $("input[tag='question']").attr('name',('question'+iQ));
 });
 
-
+$(document).on("click", "#submit", function(e) {
+    e.preventDefault();
+    completeSurvey.title = $("#title").val();
+    console.log(completeSurvey);
+ $.ajax({url: "/survey/new", type: 'post', data: JSON.stringify(completeSurvey), contentType: 'application/json', dataType: 'json' });
+  });
 
 
