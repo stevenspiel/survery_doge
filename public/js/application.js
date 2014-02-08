24cd15a4b54aad023b2238c6a49e03e53a6e2609
Steven Spiel <stevenspiel@gmail.com>
2014-02-08 17:29:33 -0600

    add rotating carousel feature to new survey forms

--- public/js/application.js
+++ public/js/application.js
@@ -16,8 +16,6 @@ $(document).ready(function () {
   }
 
   $(".answer").click(function(){
-    // console.log("MUCH CLICKS");
-
     $("#next-question").removeAttr("disabled");
     if (parseInt($(".doge-frames").css("left").replace("px", "")) === ($(".doge-item").length * -400 + 400)) {
       $("#next-question").html("Submit");
@@ -36,8 +34,6 @@ $(document).ready(function () {
   });
 
   $("#next-question").click(function(){
-    // console.log("MANY NEXT");
-    // window.Answers.
     window.Answers.answers.push($("input[type=radio]:checked").last().attr("value"));
     $(".doge-frames").css("left", "-=" + 400);
     $("#next-question").attr("disabled", true);
@@ -47,34 +43,72 @@ $(document).ready(function () {
 
   $(".doge-item").css("width", (100 / $(".doge-item").length) + "%");
 
-});
-
-
-
 // Survey creation functions
 
-$(document).on('click','#new-option',function(){
-  var iO = $('#new-question-container p').length + 1;
-  $('<p><input type="text" class="answer_new" name="answer'+iO+'" value="" placeholder="Option '+iO+'" /><a href="#" class="remove" id="remove'+iO+'"><img class="minus" alt="remove" src="/remove.png"></a> </p>').appendTo($('#new-question-container'));
-    iO++;
-    $(".remove").click(function(e){
-      $(e.currentTarget).parent().remove();
-    });
-    return false;
+  var iO = 1;
+  var iQ = 1;
+  var dogeFrame = 
+          "<div class='doge-item' id='doge-item-1'> \
+            <input type='text' class='new-question' placeholder='Question "+iQ+"' tag='question' name='question"+iQ+"'><br> \
+            <div id='options-for-question-"+iQ+"'> \
+              <p><input type='text' class='new-answer' placeholder='Option "+iO+"' name='option"+iO+"'></p> \
+              <p><input type='text' class='new-answer' placeholder='Option "+ (iO + 1) +"' name='option"+ (iO + 1) +"'></p> \
+            </div> \
+            <a href='#' id='new-option-for-question-"+iQ+"' class='next-option'><img src='/add.png' alt='add'></a><br> \
+          </div>";
+  
+  $("#new-survey").html(dogeFrame);
 });
+
+var currentQuestion = function(){
+  var leftIndent = parseInt($(".doge-frames").css('left').replace("px",""));
+  var carouselWidth = $(".doge-carousel").width()
+  return (((leftIndent * -1) / carouselWidth) + 1);
+};
+
+var numberOfQuestions = function(){
+  return ($("#new-survey .doge-item").length + 1);
+};
+
+$(document).on('click','a[id^="new-option-for-question"]',function(){
+  var iO = $('#doge-item-'+currentQuestion()+' p').length + 1;
+  $('<p><input type="text" class="answer_new" name="answer'+iO+'" value="" placeholder="Option '+iO+'" /><a href="#" class="remove" id="remove'+iO+'"><img class="minus" alt="remove" src="/remove.png"></a> </p>').appendTo($('div[id^="options-for-question"]'));
+  iO++;
+  $(".remove").click(function(e){
+    $(e.currentTarget).parent().remove();
+  });
+  return false;
+});
+
 
 $(document).on('click','#next',function(){
-  window.newSurveyHTML = $("#question-and-buttons").html();
-  var iQ = parseInt($("input[tag='question']").attr('name').replace('question',''));
-  $("#question-and-buttons").html(window.newSurveyHTML);
-  $("input[tag='question']").attr('placeholder',function(){
-    iQ ++
-    console.log(iQ);
-    return 'Question ' + iQ;
-  });
-  $("input[tag='question']").attr('name',('question'+iQ));
+  $("#previous").removeAttr('disabled');
+  $(".doge-frames").css('width', (($(".doge-item").length + 1) * 100) + "%");
+  $(".doge-item").css("width", (100 / ($(".doge-item").length + 1)) + "%");
+  $(".doge-frames").css("left", "-=" + $(".doge-carousel").width());
+  var iO = $('.doge-item-'+currentQuestion()+' p').length + 1;
+  console.log(currentQuestion());
+  console.log(numberOfQuestions());
+  if(currentQuestion() === numberOfQuestions()) {
+    var dogeFrame = 
+      "<div class='doge-item' id='doge-item-"+currentQuestion()+"'> \
+        <input type='text' class='new-question' placeholder='Question "+currentQuestion()+"' tag='question' name='question"+currentQuestion()+"'><br> \
+        <div id='options-for-question-"+currentQuestion()+"'> \
+          <p><input type='text' class='new-answer' placeholder='Option "+iO+"' name='"+ currentQuestion() + '-' + iO +"'></p> \
+          <p><input type='text' class='new-answer' placeholder='Option "+ (iO + 1) +"' name='"+ currentQuestion() + '-' + iO +"'></p> \
+        </div> \
+        <a href='#' id='new-option-for-question-"+currentQuestion()+"' class='next-option'><img src='/add.png' alt='add'></a><br> \
+      </div>";
+    $(dogeFrame).appendTo($("#new-survey"));
+  };
 });
 
-
-
-
+$(document).on('click','#previous',function(){
+  $(".doge-frames").css('width', (($(".doge-item").length + 1) * 100) + "%");
+  $(".doge-item").css("width", (100 / ($(".doge-item").length + 1)) + "%");
+  $(".doge-frames").css("left", "+=" + $(".doge-carousel").width());
+  if(currentQuestion() === 1){
+    console.log($("#previous"));
+    $("#previous").attr('disabled','disabled');
+  }
+});
