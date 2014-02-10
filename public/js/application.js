c6f8e99e2dd6ff69c08919e0316f06e14d4ee7c1
Sam Blackman <sblackman@gmail.com>
2014-02-10 16:58:59 -0600

    Pair with Mike and refactor some of JS into OO patterns

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,44 @@
+var completeSurvey = {};
+
+var Survey = {
+  questions: [],
+  addQuestion: function(question) {
+    this.questions.push(question);
+  },
+  createQuestion: function(e){
+    debugger
+    // console.log("Length of new answers is" + $(".new-answer").length);
+    question = new Question($(".new-question ").val());
+    $(".new-answer").each(function(i, e) {
+      question.addAnswer($(e).val());
+    });
+    this.addQuestion(question);
+    $(document).trigger("question:created", question);
+  },
+  resetForm: function() {
+    // completeSurvey[q_name] = questionsArray;
+    window.newSurveyHTML = $("#new-question-container").html();
+    var iQ = parseInt($("input[tag='question']").attr('name').replace('question',''));
+    $("#new-question-container").html(window.newSurveyHTML);
+    $("input[tag='question']").attr('placeholder',function(){
+      iQ ++
+      console.log(iQ);
+      return 'Question ' + iQ;
+    });
+    $("input[tag='question']").attr('name',('question'+iQ));
+  }
+}
+
+function Question(text) {
+  this.text = text;
+  this.answers = [];
+}
+
+Question.prototype.addAnswer = function(answer) {
+  this.answers.push(answer);
+}
+
+
 $(document).ready(function () {
 
   // send an HTTP DELETE request for the sign-out link
@@ -44,59 +85,45 @@ $(document).ready(function () {
   $(".doge-frames").css("width", ($(".doge-item").length * 100) + "%");
 
   $(".doge-item").css("width", (100 / $(".doge-item").length) + "%");
+
+  $("#question-and-buttons #new-option").on('click',function(){
+    var iO = $('#new-question-container p').length + 1;
+    $('<p><input type="text" class="new-answer" name="answer'+iO+'" value="" placeholder="Option '+iO+'" /><a href="#" class="remove" id="remove'+iO+'"><img class="minus" alt="remove" src="/remove.png"></a> </p>').appendTo($('#new-question-container'));
+    iO++;
+    return false;
+  });
+
+  $("#question-and-buttons").on('click','p .remove',function(e){
+    console.log(e);
+    $(e.currentTarget).parent().remove();
+  });
+
+  $('#next').on('click', Survey.createQuestion.bind(Survey));
+  $(document).on('question:created', function(e, question) {
+    console.log(question);
+  });
+
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
+    completeSurvey.title = $("#title").val();
+    $.ajax({
+      url: "/survey/new",
+      type: 'post',
+      data: JSON.stringify(completeSurvey),
+      success: function(response){
+        window.location.href = "/user/" + response.user_id;
+      },
+      contentType: 'application/json',
+      dataType: 'json'
+    });
+  });
 });
 
 
 
 // Survey creation functions
 
-var completeSurvey = {};
 
-var questionsArray = [];
 
-$(document).on('click','#new-option',function(){
-  var iO = $('#new-question-container p').length + 1;
-  $('<p><input type="text" class="new-answer" name="answer'+iO+'" value="" placeholder="Option '+iO+'" /><a href="#" class="remove" id="remove'+iO+'"><img class="minus" alt="remove" src="/remove.png"></a> </p>').appendTo($('#new-question-container'));
-  iO++;
-  return false;
-});
-
-$(document).on('click','#question-and-buttons p .remove',function(e){
-  console.log(e);
-  $(e.currentTarget).parent().remove();
-});
-
-$(document).on('click','#next',function(){
-  var q_name = $(".new-question ").val();
-  console.log("Length of new answers is" + $(".new-answer").length);
-  for (var i = 0; i < $(".new-answer").length; i++) {;
-    questionsArray[i] = $($(".new-answer")[i]).val();
-  };
-  completeSurvey[q_name] = questionsArray;
-  window.newSurveyHTML = $("#new-question-container").html();
-  var iQ = parseInt($("input[tag='question']").attr('name').replace('question',''));
-  $("#new-question-container").html(window.newSurveyHTML);
-  $("input[tag='question']").attr('placeholder',function(){
-    iQ ++
-    console.log(iQ);
-    return 'Question ' + iQ;
-  });
-  $("input[tag='question']").attr('name',('question'+iQ));
-});
-
-$(document).on("click", "#submit", function(e) {
-  e.preventDefault();
-  completeSurvey.title = $("#title").val();
-  $.ajax({
-    url: "/survey/new",
-    type: 'post',
-    data: JSON.stringify(completeSurvey),
-    success: function(response){
-      window.location.href = "/user/" + response.user_id;
-    },
-    contentType: 'application/json',
-    dataType: 'json'
-  });
-});
 
 
